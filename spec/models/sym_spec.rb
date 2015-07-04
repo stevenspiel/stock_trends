af905c1de545bfaa313cafaee1c928833b3492ee
Steven Spiel <stevenspiel@gmail.com>
2015-07-04 16:06:07 -0400

    add sym spec

---
+++ spec/models/sym_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe Sym do
+  describe 'associations' do
+    it { should belong_to(:market) }
+    it { should have_many(:days).order(:date).dependent(:destroy) }
+    it { should have_many(:ticks).through(:days) }
+    it { should have_many(:historical_datums).order(:date).dependent(:destroy) }
+  end
+
+  describe 'to_s' do
+    let(:subject) { described_class.new(name: 'foo') }
+    it 'returns the name' do
+      expect(subject.to_s).to eq(subject.name)
+    end
+  end
+end
