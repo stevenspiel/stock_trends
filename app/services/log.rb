ad638659a1b127b5ff307f9c482bffaa00d72b13
Steven Spiel <stevenspiel@gmail.com>
2015-07-13 20:28:38 -0400

    log by order of sym name, and not by markets

--- app/services/log.rb
+++ app/services/log.rb
@@ -10,60 +10,54 @@ class Log
     ActiveRecord::Base.logger = nil # suppress sql output
   end
 
-  def intraday(api: @yf, scope: :all, where: nil)
+  def intraday(api: @yf)
     # cron runs nightly
-    @markets.each do |market|
+    Sym.ordered.find_each do |sym|
       # First tick is opening price and last one is closing price
-      market.syms.send(scope).where(where).find_each do |sym|
-        begin
-          print "Logging Intraday Ticks for #{sym.market} - #{sym.padded}"
-          success = api.log_intraday_history(sym)
-          sym.reset_cached_data(:five_weeks, :historical_data, :min_and_max_historical_dates) if success
-        rescue => e
-          binding.pry if Rails.env.development?
-        end
-        puts percent_complete(scope: :greater_than, sym: sym)
+      begin
+        print "Logging Intraday Ticks for #{sym.padded}"
+        success = api.log_intraday_history(sym)
+        sym.reset_cached_data(:five_weeks, :historical_data, :min_and_max_historical_dates) if success
+      rescue => e
+        binding.pry if Rails.env.development?
       end
-      Curator.run!(market)
+      puts percent_complete(scope: :greater_than, sym: sym)
+    end
+    @markets.each { |market| Curator.run!(market) }
+    nil
+  end
+
+  def prices(api: @yf)
+    Sym.ordered.in_groups_of(BATCH_SIZE) do |group|
+      begin
+        symbols = group.compact.map(&:name)
+        prices = api.prices(symbols)
+        prices.each do |quote|
+          ask = quote[:ask]
+          next unless ask.present?
+          sym = Sym.find_by(name: quote[:symbol])
+          sym.update_attributes!(current_price: ask)
+          puts "#{sym.padded} - $#{sym.current_price}"
+        end
+      rescue => e
+        puts e
+        binding.pry if Rails.env.development?
+      end
     end
     nil
   end
 
-  def prices(api: @yf, scope: :all, where: [])
-    @markets.each do |market|
-      market.syms.send(scope).where(where).in_groups_of(BATCH_SIZE) do |group|
-        begin
-          symbols = group.compact.map(&:name)
-          prices = api.prices(symbols)
-          prices.each do |quote|
-            ask = quote[:ask]
-            next unless ask.present?
-            sym = Sym.find_by(name: quote[:symbol])
-            sym.update_attributes!(current_price: ask)
-            puts "#{sym.padded} - $#{sym.current_price}"
-          end
-        rescue => e
-          puts e
-          binding.pry if Rails.env.development?
-        end
+  def historical(api: @q)
+    Sym.pending_historical.ordered.find_each do |sym|
+      begin
+        print "Logging Historical Data for #{sym.padded('.')}..."
+        result = api.log_historical(sym)
+        return if api.not_successful?(result) && api.handle_error(result, sym)
+        sym.reset_cached_data(:historical_data)
+      rescue => e
+        binding.pry if Rails.env.development?
       end
-    end
-    nil
-  end
-
-  def historical(api: @q, scope: :pending_historical, where: nil)
-    @markets.each do |market|
-      market.syms.send(scope).where(where).find_each do |sym|
-        begin
-          print "Logging Historical Data for #{sym.market} - #{sym.padded('.')}..."
-          result = api.log_historical(sym)
-          return if api.not_successful?(result) && api.handle_error(result, sym)
-          sym.reset_cached_data(:historical_data)
-        rescue => e
-          binding.pry if Rails.env.development?
-        end
-        puts percent_complete(scope: scope)
-      end
+      puts percent_complete(scope: :pending_historical)
     end
     nil
   end
