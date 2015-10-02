9514b5657f3acb5dfb95d90ddf93e5e34584ac2a
Steven Spiel <stevenspiel@gmail.com>
2015-10-02 11:42:36 -0400

    only precompile scss and coffee files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.assets.precompile += %w( *.css *.js *.css.scss *js.coffee )
+Rails.application.config.assets.precompile += %w( *.css.scss *js.coffee )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
